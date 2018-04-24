aab638c24c4d3911633ba7a702f9b44437025e19
Anand Siddharth <anandsiddharth@bitbucket.org>
2018-04-24 08:36:32 +0530

    social grant created

---
+++ src/Grant/SocialGrant.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Anand\Grant;
+
+use League\OAuth2\Server\Grant\AbstractGrant;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use League\OAuth2\Server\Entities\UserEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Exception\OAuthServerException;
+use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
+use League\OAuth2\Server\Repositories\UserRepositoryInterface;
+/**
+ * Social grant class.
+ */
+
+class SocialGrant extends AbstractGrant {
+	/**
+	* @param UserRepositoryInterface         $userRepository
+	* @param RefreshTokenRepositoryInterface $refreshTokenRepository
+	*/
+	public function __construct(
+		UserRepositoryInterface $userRepository,
+		RefreshTokenRepositoryInterface $refreshTokenRepository
+	) {
+		$this->setUserRepository($userRepository);
+		$this->setRefreshTokenRepository($refreshTokenRepository);
+
+		$this->refreshTokenTTL = new \DateInterval('P1M');
+	}
+	
+	/**
+	* {@inheritdoc}
+	*/
+	public function respondToAccessTokenRequest(
+		ServerRequestInterface $request,
+		ResponseTypeInterface $responseType,
+		\DateInterval $accessTokenTTL
+	) {
+		// Validate request
+		$client = $this->validateClient($request);
+		$scopes = $this->validateScopes($this->getRequestParameter('scope', $request, $this->defaultScope));
+		$user = $this->validateUser($request, $client);
+
+		// Finalize the requested scopes
+		$finalizedScopes = $this->scopeRepository->finalizeScopes($scopes, $this->getIdentifier(), $client, $user->getIdentifier());
+
+		// Issue and persist new tokens
+		$accessToken = $this->issueAccessToken($accessTokenTTL, $client, $user->getIdentifier(), $finalizedScopes);
+		$refreshToken = $this->issueRefreshToken($accessToken);
+
+		// Inject tokens into response
+		$responseType->setAccessToken($accessToken);
+		$responseType->setRefreshToken($refreshToken);
+
+		return $responseType;
+	}
+
+	    /**
+     * @param ServerRequestInterface $request
+     * @param ClientEntityInterface  $client
+     *
+     * @throws OAuthServerException
+     *
+     * @return UserEntityInterface
+     */
+	protected function validateUser(ServerRequestInterface $request, ClientEntityInterface $client)
+	{
+		$accessToken = $this->getRequestParameter('accessToken', $request);
+		if (is_null($username)) {
+		  throw OAuthServerException::invalidRequest('accessToken');
+		}
+
+		$provider = $this->getRequestParameter('provider', $request);
+		if (is_null($password)) {
+		  throw OAuthServerException::invalidRequest('provider');
+		}
+
+		$user = $this->userRepository->getUserEntityByUserProvider(
+		  $username,
+		  $password,
+		  $this->getIdentifier(),
+		  $client
+		);
+		if ($user instanceof UserEntityInterface === false) {
+		  $this->getEmitter()->emit(new RequestEvent(RequestEvent::USER_AUTHENTICATION_FAILED, $request));
+
+		  throw OAuthServerException::invalidCredentials();
+		}
+
+		return $user;
+	}
+
+
+	/**
+	* {@inheritdoc}
+	*/
+	public function getIdentifier()
+	{
+		return 'social';
+	}
+}
